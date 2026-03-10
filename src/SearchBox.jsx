5857fb9b0b86401844eec4e62ea77e633ae21683
Amit-1424 <amit1424.work@gmail.com>
2026-03-11 01:36:04 +0530

    fetch weatherData

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,12 +2,23 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css"
 import { useState, useSyncExternalStore } from 'react';
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 
 export default function SearchBox(){
 
     let [city, setCity] = useState("");
     
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+    let getWeatherData =  async () => {
+        let response = await fetch (
+            `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        let jsonResponse = await response.json();
+        console.log(jsonResponse)
+    }
+
     let handleChange = (event) => {
         setCity(event.target.value)
     }
@@ -16,7 +27,7 @@ export default function SearchBox(){
         event.preventDefault();
         console.log({city})
         setCity("");
-
+        getWeatherData();
     }
 
     return (
